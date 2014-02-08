e28c3face589c5f197495b21fcb4500317f335b5
Anubhaw Arya <anubhaw.arya@gmail.com>
2014-02-08 11:49:25 -0500

    Registering stuff

--- AssassinAndroid/src/com/AssassinAndroid/Activities/RegisterActivity.java
+++ AssassinAndroid/src/com/AssassinAndroid/Activities/RegisterActivity.java
@@ -3,8 +3,7 @@ package com.AssassinAndroid.Activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.*;
 
 /**
  * User: AnubhawArya
@@ -13,7 +12,8 @@ import android.widget.Spinner;
  */
 public class RegisterActivity extends Activity {
     EditText mEmail, mPassword, mConfirmPassword, mAge, mRace, mFeet, mInches;
-    Spinner mGender;
+    TextView mErrorText;
+    RadioButton mMale, mFemale;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,18 +21,63 @@ public class RegisterActivity extends Activity {
         mEmail = (EditText) findViewById(R.id.mEmail);
         mPassword = (EditText) findViewById(R.id.mPassword);
         mConfirmPassword = (EditText) findViewById(R.id.mConfirmPassword);
-        mGender = (Spinner) findViewById(R.id.mGender);
+        mMale = (RadioButton) findViewById(R.id.mMale);
+        mFemale = (RadioButton) findViewById(R.id.mFemale);
         mAge = (EditText) findViewById(R.id.mAge);
         mRace = (EditText) findViewById(R.id.mRace);
         mFeet = (EditText) findViewById(R.id.mFeet);
         mInches = (EditText) findViewById(R.id.mInches);
+        mErrorText = (TextView) findViewById(R.id.mErrorText);
         findViewById(R.id.mRegister).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String email, password, race, gender;
+                String email = "", password = "", race = "", gender = "";
                 int age, feet, inches;
-                email=mEmail.getText().toString();
-                if(mPassword.getText().toString().equals(mConfirmPassword.getText().toString()))
-                    password=mPassword.getText().toString();
+                if (mEmail.getText().toString().matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"))
+                    email = mEmail.getText().toString();
+                else {
+                    mErrorText.setText("Invalid Email");
+                    return;
+                }
+                if (!mPassword.getText().toString().equals("") &&
+                        mPassword.getText().toString().equals(mConfirmPassword.getText().toString()))
+                    password = mPassword.getText().toString();
+                else {
+                    mErrorText.setText("Invalid Password");
+                    return;
+                }
+                if (mMale.isChecked())
+                    gender = "Male";
+                else if (mFemale.isChecked())
+                    gender = "Female";
+                else {
+                    mErrorText.setText("Invalid Gender");
+                    return;
+                }
+                if (!mAge.getText().toString().equals(""))
+                    age = Integer.parseInt(mAge.getText().toString());
+                else {
+                    mErrorText.setText("Invalid Age");
+                    return;
+                }
+                if (!mRace.getText().toString().equals(""))
+                    race = mRace.getText().toString();
+                else {
+                    mErrorText.setText("Invalid Race");
+                    return;
+                }
+                if (!mFeet.getText().toString().equals(""))
+                    feet = Integer.parseInt(mFeet.getText().toString());
+                else {
+                    mErrorText.setText("Invalid Feet");
+                    return;
+                }
+                if (!mInches.getText().toString().equals(""))
+                    inches = Integer.parseInt(mInches.getText().toString());
+                else {
+                    mErrorText.setText("Invalid Inches");
+                    return;
+                }
+                mErrorText.setText("");
             }
         });
     }
