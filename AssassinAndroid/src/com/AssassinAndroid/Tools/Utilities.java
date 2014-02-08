cc95a217648363284161022fb906e9de572d44d3
Anubhaw Arya <anubhaw.arya@gmail.com>
2014-02-08 15:27:39 -0500

    Registering with Google Play Services

--- AssassinAndroid/src/com/AssassinAndroid/Tools/Utilities.java
+++ AssassinAndroid/src/com/AssassinAndroid/Tools/Utilities.java
@@ -1,12 +1,22 @@
 package com.AssassinAndroid.Tools;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+import com.AssassinAndroid.Activities.TargetActivity;
 import com.AssassinAndroid.Service.LocationAlarm;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.io.IOException;
 
 /**
  * User: AnubhawArya
@@ -14,7 +24,14 @@ import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
  * Time: 10:51 PM
  */
 public class Utilities {
+
     private static ImageLoader mImageLoader;
+
+    public static final String PROPERTY_REG_ID = "registration_id";
+    static String SENDER_ID = "580453532961";
+    static final String TAG = "AndroidAssassin";
+    static GoogleCloudMessaging gcm;
+    static String regid;
 
     public static boolean isNetworkAvailable(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -29,6 +46,14 @@ public class Utilities {
     public static void init(Context context) {
         setupImageLoader(context);
         setupAlarms(context);
+        if (checkPlayServices(context)) {
+            gcm = GoogleCloudMessaging.getInstance(context);
+            regid = getRegistrationId(context);
+            if (regid.isEmpty())
+                registerInBackground(context);
+            Toast.makeText(context, regid, Toast.LENGTH_SHORT).show();
+        } else
+            Log.i(TAG, "No valid Google Play Services APK found.");
     }
 
     private static void setupImageLoader(Context context) {
@@ -42,5 +67,90 @@ public class Utilities {
 
     private static void setupAlarms(Context context) {
         new LocationAlarm().setLocationAlarm(context);
+    }
+
+    public static boolean checkPlayServices(Context context) {
+        return GooglePlayServicesUtil.isGooglePlayServicesAvailable(context) == ConnectionResult.SUCCESS ? true : false;
+    }
+
+    private static String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+        return registrationId;
+    }
+
+
+    private static SharedPreferences getGCMPreferences(Context context) {
+        // This sample app persists the registration ID in shared preferences, but
+        // how you store the regID in your app is up to you.
+        return context.getSharedPreferences(TargetActivity.class.getSimpleName(), Context.MODE_PRIVATE);
+    }
+
+    /**
+     * Registers the application with GCM servers asynchronously.
+     * <p/>
+     * Stores the registration ID and app versionCode in the application's
+     * shared preferences.
+     */
+    private static void registerInBackground(final Context context) {
+        new AsyncTask() {
+            protected Object doInBackground(Object[] params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+                    regid = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regid;
+
+                    // You should send the registration ID to your server over HTTP,
+                    // so it can use GCM/HTTP or CCS to send messages to your app.
+                    // The request to your server should be authenticated if your app
+                    // is using accounts.
+                    sendRegistrationIdToBackend();
+
+                    // For this demo: we don't need to send it because the device
+                    // will send upstream messages to a server that echo back the
+                    // message using the 'from' address in the message.
+
+                    // Persist the regID - no need to register again.
+                    storeRegistrationId(context, regid);
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                    // If there is an error, don't just keep trying to register.
+                    // Require the user to click a button again, or perform
+                    // exponential back-off.
+                }
+                return msg;
+            }
+        }.execute(null, null, null);
+    }
+
+    /**
+     * Sends the registration ID to your server over HTTP, so it can use GCM/HTTP
+     * or CCS to send messages to your app. Not needed for this demo since the
+     * device sends upstream messages to a server that echoes back the message
+     * using the 'from' address in the message.
+     */
+    private static void sendRegistrationIdToBackend() {
+        // Your implementation here.
+    }
+
+    /**
+     * Stores the registration ID and app versionCode in the application's
+     * {@code SharedPreferences}.
+     *
+     * @param context application's context.
+     * @param regId   registration ID
+     */
+    private static void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.commit();
     }
 }
