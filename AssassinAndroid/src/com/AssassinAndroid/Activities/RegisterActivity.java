ad98e6bde91b68523e9e3cc87b5336eee43911fd
Anubhaw Arya <anubhaw.arya@gmail.com>
2014-02-08 12:39:01 -0500

    Images during registration

--- AssassinAndroid/src/com/AssassinAndroid/Activities/RegisterActivity.java
+++ AssassinAndroid/src/com/AssassinAndroid/Activities/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.AssassinAndroid.Activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
@@ -11,9 +13,20 @@ import android.widget.*;
  * Time: 11:06 AM
  */
 public class RegisterActivity extends Activity {
+
+    private static final int CAMERA_REQUEST = 1888;
     EditText mEmail, mPassword, mConfirmPassword, mAge, mRace, mFeet, mInches;
     TextView mErrorText;
     RadioButton mMale, mFemale;
+    ImageView mImageOne, mImageTwo, mImageThree;
+    Bitmap one, two, three;
+    LinearLayout mImageLayout;
+    View.OnClickListener mImageOnClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        }
+    };
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +41,14 @@ public class RegisterActivity extends Activity {
         mFeet = (EditText) findViewById(R.id.mFeet);
         mInches = (EditText) findViewById(R.id.mInches);
         mErrorText = (TextView) findViewById(R.id.mErrorText);
+        mImageOne = (ImageView) findViewById(R.id.mImageOne);
+        mImageTwo = (ImageView) findViewById(R.id.mImageTwo);
+        mImageThree = (ImageView) findViewById(R.id.mImageThree);
+        mImageOne.setTag(false);
+        mImageTwo.setTag(false);
+        mImageThree.setTag(false);
+        mImageLayout = (LinearLayout) findViewById(R.id.mImageLayout);
+        mImageLayout.setOnClickListener(mImageOnClickListener);
         findViewById(R.id.mRegister).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String email = "", password = "", race = "", gender = "";
@@ -77,8 +98,37 @@ public class RegisterActivity extends Activity {
                     mErrorText.setText("Invalid Inches");
                     return;
                 }
+                if (one == null) {
+                    mErrorText.setText("Need photo #1");
+                    return;
+                }else if (two == null) {
+                    mErrorText.setText("Need photo #2");
+                    return;
+                }else if (three == null) {
+                    mErrorText.setText("Need photo #3");
+                    return;
+                }
                 mErrorText.setText("");
             }
         });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            if (!(Boolean) mImageOne.getTag()) {
+                mImageOne.setTag(true);
+                one = (Bitmap) data.getExtras().get("data");
+                mImageOne.setImageBitmap(one);
+            } else if (!(Boolean) mImageTwo.getTag()) {
+                mImageTwo.setTag(true);
+                two = (Bitmap) data.getExtras().get("data");
+                mImageTwo.setImageBitmap(two);
+            } else if (!(Boolean) mImageThree.getTag()) {
+                mImageThree.setTag(true);
+                three = (Bitmap) data.getExtras().get("data");
+                mImageThree.setImageBitmap(three);
+                mImageLayout.setOnClickListener(null);
+            }
+        }
     }
 }
