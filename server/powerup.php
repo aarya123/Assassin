faaba13cd23084f67d69911b47f68de496ba1ee4
Dan Green <dan.singkiwi@gmail.com>
2014-02-08 22:28:41 -0500

    Powerups Added

--- server/powerup.php
+++ server/powerup.php
@@ -8,6 +8,34 @@
 		$dbh = new PDO($conn_str, $user, $password);
 		$result = messageUser($dbh, $_POST['user_id']);
 		print $result;
+
+		if(array_key_exists('powerup', $_POST)) {
+			$powerup = array('powerup', $_POST);
+			if($powerup == 0) {
+				print 'Invisibility Powerup Activated';
+				$sql = 'SELECT id FROM users';
+				while($row = fetch_assoc(mysql_query($sql))) {
+					$target = 'SELECT target FROM users WHERE userid="$row[id]"';
+					if($target == $userid) {
+						messageUser($dbh, $_POST['$row[id]'], ['Your target activated an invisibility powerup!']);
+					}
+				}
+			} 
+			else if($powerup == 1) {
+				print 'Radar Powerup Activated';
+				$target = array('target', $_POST);
+				messageUser($dbh, $_POST[$target], ['Your target activated a radar powerup!']);
+				$sql = 'SELECT id FROM users';
+				while($row = fetch_assoc(mysql_query($sql))) {
+					$target_id = 'SELECT id FROM users WHERE user_id = :user_id';
+					if($target_id == $target) {
+						$longitude = mysql_fetch_row('longitude');
+						$latitude = mysql_fetch_row('latitude');
+					}
+					break;
+				}
+			}
+		}
 	}
 	catch(Exception $e) {
 		print json_encode(array('error' => $e->getMessage()));
